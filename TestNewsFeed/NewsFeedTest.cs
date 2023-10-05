45aa0f26ee5d842994c6da0f7a9409ee61ea8ca9
ManjeetR011 <ranamanjeet01@gmail.com>
2023-10-05 18:52:24 +0530

    NEw changes

--- TestNewsFeed/NewsFeedTest.cs
+++ TestNewsFeed/NewsFeedTest.cs
@@ -13,54 +13,33 @@ namespace TestNewsFeedApp
         private StoryService storyService;
         private List<Story> stories;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        posts = new List<Story>()
-    //        {
-    //          new Story() {
-    //               Id = 1,
-    //               Author = "justin",
-    //               Title = "Justin.tv is looking for a Lead Flash Engineer!",
-    //               Descendents = 16,
-    //               PollCount = 6,
-    //               Description = "Justin.tv is the biggest live video site online. We serve hundreds of thousands of video streams a day, and have supported up to 50k live concurrent viewers. Our site is growing every week, and we just added a 10 gbps line to our colo. Our unique visitors are up 900% since January.<p>There are a lot of pieces that fit together to make Justin.tv work: our video cluster, IRC server, our web app, and our monitoring and search services, to name a few. A lot of our website is dependent on Flash, and we're looking for talented Flash Engineers who know AS2 and AS3 very well who want to be leaders in the development of our Flash.<p>Responsibilities<p><pre><code>    * Contribute to product design and implementation discussions\n    * Implement projects from the idea phase to production\n    * Test and iterate code before and after production release \n</code></pre>\nQualifications<p><pre><code>    * You should know AS2, AS3, and maybe a little be of Flex.\n    * Experience building web applications.\n    * A strong desire to work on website with passionate users and ideas for how to improve it.\n    * Experience hacking video streams, python, Twisted or rails all a plus.\n</code></pre>\nWhile we're growing rapidly, Justin.tv is still a small, technology focused company, built by hackers for hackers. Seven of our ten person team are engineers or designers. We believe in rapid development, and push out new code releases every week. We're based in a beautiful office in the SOMA district of SF, one block from the caltrain station. If you want a fun job hacking on code that will touch a lot of people, JTV is for you.<p>Note: You must be physically present in SF to work for JTV. Completing the technical problem at <a href=\"http://www.justin.tv/problems/bml\" rel=\"nofollow\">http://www.justin.tv/problems/bml</a> will go a long way with us. Cheers!",
-    //               CreatedAt = new DateTime(2008,05,17,05,10,17),
-    //               Type = "job",
-    //           },
-    //          new Story() {
-    //               Id = 2,
-    //               Author = "John",
-    //               Title = "Intersting facts about",
-    //               Descendents = 16,
-    //               PollCount = 6,
-    //               Description = "Yoga and meditation can change your life",
-    //               CreatedAt = new DateTime(2007,04,07,08,13,07),
-    //               Type = "job",
-    //           }
-    //        };
-    //        postRepo = new Mock<IPostRepository>();
-    //        postService = new PostService(postRepo.Object);
+        [SetUp]
+        public void SetUp()
+        {
+            stories = new List<Story>()
+            {
+              new Story() {
+                   Id = 1,
+                   By = "justin",
+                   Title = "justin.tv is looking for a lead flash engineer!",
+                  Descendents = 16,
+                   Text = "justin.tv is the biggest live video site online. we serve hundreds of thousands of video streams a day, and have supported up to 50k live concurrent viewers. our site is growing every week, and we just added a 10 gbps line to our colo. our unique visitors are up 900% since january.<p>there are a lot of pieces that fit together to make justin.tv work: our video cluster, irc server, our web app, and our monitoring and search services, to name a few. a lot of our website is dependent on flash, and we're looking for talented flash engineers who know as2 and as3 very well who want to be leaders in the development of our flash.<p>responsibilities<p><pre><code>    * contribute to product design and implementation discussions\n    * implement projects from the idea phase to production\n    * test and iterate code before and after production release \n</code></pre>\nqualifications<p><pre><code>    * you should know as2, as3, and maybe a little be of flex.\n    * experience building web applications.\n    * a strong desire to work on website with passionate users and ideas for how to improve it.\n    * experience hacking video streams, python, twisted or rails all a plus.\n</code></pre>\nwhile we're growing rapidly, justin.tv is still a small, technology focused company, built by hackers for hackers. seven of our ten person team are engineers or designers. we believe in rapid development, and push out new code releases every week. we're based in a beautiful office in the soma district of sf, one block from the caltrain station. if you want a fun job hacking on code that will touch a lot of people, jtv is for you.<p>note: you must be physically present in sf to work for jtv. completing the technical problem at <a href=\"http://www.justin.tv/problems/bml\" rel=\"nofollow\">http://www.justin.tv/problems/bml</a> will go a long way with us. cheers!",
+                   Type = "story",
+               }
+            };
+            storyService = new StoryService();
 
-    //    }
+        }
 
-    //    [Test]
-    //    public async Task TestGetPosts()
-    //    {
-    //        postRepo.Setup(x => x.GetPosts()).ReturnsAsync(posts);
-            
-    //        var result =  await postService.GetPosts();
-    //        Assert.NotZero(result.Count);
-    //    }
+        [Test]
+        public async Task testgetposts()
+        {
+           
+            var result =  await storyService.GetNewStories();
+            Assert.NotZero(result.Count);
+        }
 
-    //    [Test]
-    //    public async Task TestSearch()
-    //    {
-    //        postRepo.Setup(x => x.Search(It.IsAny<string>(),It.IsAny<int>())).ReturnsAsync(posts);
-
-    //        var result = await postService.Search("Just",1);
-    //        Assert.NotZero(result.Count);
-    //    }
+       
 
     }
 }
